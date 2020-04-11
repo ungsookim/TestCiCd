c9a0d8ced00eeb817d246e367420ae1d3b991104
skyer9 <skyer9@gmail.com>
2020-04-11 12:17:09 +0900

    first commit

---
+++ src/main/java/kr/co/episode/TestCiCd/TestCiCdApplication.java
@@ -0,0 +1,13 @@
+package kr.co.episode.TestCiCd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/episode/TestCiCd/TestCiCdApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.episode.TestCiCd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestCiCdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
